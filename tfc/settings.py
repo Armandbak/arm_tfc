7f2b12154919ed7c89111c5f0735d3a20b5152d3
Armandbak <julienmugisha206.com>
2024-06-10 06:07:06 +0200

    first commit

--- tfc/settings.py
+++ tfc/settings.py
@@ -94,15 +94,13 @@ WSGI_APPLICATION = 'tfc.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'ArmandTfc',
-        'USER': 'postgres',
-        'PASSWORD': 'Armandbak',
-        'HOST': 'localhost',
-        'PORT': '5432',
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
